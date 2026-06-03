2146e7df4127158fbc6cf54b13c39c180aa6ccc4
leohirt <leonardo.hirt@noseryoung.ch>
2026-06-03 16:09:39 +0200

    added basic routing setup

---
+++ my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Routes>
+    </Routes>
+      <h1>Welcome to Rest Foods!</h1>
+    </>
+  );
+}
+
+export default App
