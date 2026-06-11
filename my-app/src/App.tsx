314a9b0f210806d97dac2c629d58bf687777e3cb
ny-david-s <david.stupar@noseryoung.com>
2026-06-11 12:08:06 +0200

    Update App.tsx
    - Add Nav to ReservationPage.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./Pages/MainPage";
 import MenuPage from "./Pages/MenuPage";
+import ReservationPage from "./Pages/ReservationPage";
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/menu" element={<MenuPage />} />
+        <Route path="/reservation" element={<ReservationPage />} />
+
       </Routes>
     </>
   );
